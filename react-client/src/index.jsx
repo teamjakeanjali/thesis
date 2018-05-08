b2dc85b5b11c82b3016dc16e00115880393127ea
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-05-07 22:47:24 -0500

    google working

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,36 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import $ from "jquery";
+import List from "./components/List.jsx";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       items: []
-    }
+    };
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
+    // axios.post("/audio").then();
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  render() {
+    return (
+      <div>
+        <h1>Item List</h1>
+        <List items={this.state.items} />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
